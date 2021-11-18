76a18758b63b716c39ba2c328e16c4271d114029
David Holmqvist <dicci0308@gmail.com>
2021-11-18 09:51:09 +0100

    clean digital space

--- init.sh
+++ init.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-for config in foot waybar river emacs kak alacritty tmux nvim sway
+for config in mako kanshi foot waybar river kak tmux
 do
     mkdir -p ~/.config/"$config"
     stow "$config" -t ~/.config/"$config"
 done
 
 stow zsh
-stow task
 stow scripts -t ~/.local/bin
 stow chromium -t ~/.config
 stow wallpapers -t ~/Pictures
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -8,3 +8,6 @@ bindkey -e
 
 setopt hist_ignore_all_dups
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+eval "$(zoxide init zsh)"
+
+alias battery="cat /sys/class/power_supply/BAT0/capacity"
